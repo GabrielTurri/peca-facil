84c1b8029ef96b53e43729005dddbb69349c589d
Felipe Haddad <felipehaddad25@gmail.com>
2024-11-18 21:44:43 -0300

    dropdowns com os tipos de hardwares e perifericos do json

--- src/index.php
+++ src/index.php
@@ -1,3 +1,33 @@
+<?php
+  $json = file_get_contents("produtos.json");
+
+  if ($json === false) {
+    die("Erro ao ler o json");
+  }
+
+  $produtos = json_decode($json, true);
+  if ($produtos === null){
+    die("Erro ao decodificar produtos");
+  }
+
+  $tipos_hardwares = [];
+  $tipos_perifericos = [];
+  // Procurar o produto pela categoria
+  foreach ($produtos as $produto) { 
+    if($produto['categoria'] == "hardware"){
+      if(in_array($produto['tipo'], $tipos_hardwares)){
+      } else{
+        array_push($tipos_hardwares, $produto['tipo']);
+      }
+    } else if($produto['categoria'] == "periferico"){
+      if(in_array($produto['tipo'], $tipos_perifericos)){
+      } else{
+        array_push($tipos_perifericos, $produto['tipo']);
+      }
+    }
+  }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -37,8 +67,36 @@
 
   </div>
   <div class="d-flex flex-row justify-content-around gap-2 my-4">
-    <a href="#">Hardware</a>
-    <a href="#">Periféricos</a>
+
+    <div class="dropdown">
+      <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        Hardware
+      </button>
+      <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+      <?php
+        foreach($tipos_hardwares as $hardware){
+          echo "<a class='dropdown-item' href='#'>{$hardware}</a>
+          ";
+        }
+      ?>
+
+      </div>
+    </div>
+
+    <div class="dropdown">
+      <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        Periféricos
+      </button>
+      <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+      <?php
+        foreach($tipos_perifericos as $periferico){
+          echo "<a class='dropdown-item' href='#'>{$periferico}</a>
+          ";
+        }
+      ?>
+
+      </div>
+    </div>
     <a href="#">Ofertas</a>
     <a href="#">Cupons</a>
     <a href="#">Lojas Parceiras</a>
@@ -47,16 +105,6 @@
   <h2>Produtos em destaque</h2>
   <div class="d-flex flex-row flex-wrap gap-2 my-2 justify-content-center">
   <?php
-    $json = file_get_contents("produtos.json");
-
-    if ($json === false) {
-      die("Erro ao ler o json");
-    }
-
-    $produtos = json_decode($json, true);
-    if ($produtos === null){
-      die("Erro ao decodificar produtos");
-    }
 
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
       $valorProcurado = $_POST['busca'];
@@ -112,7 +160,7 @@
         $preco_medio = 0;
         $qtde_lojas = 0;
         $menor_preco = 99999;
-        $loja_menor_preco = "";
+        $loja_menor_preco = ""; 
         
         foreach($produto['lojas'] as $loja){
           $soma_preco += $loja['preco'];
@@ -138,5 +186,8 @@
   </div>
 
   <footer></footer>  
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </body>
 </html>
