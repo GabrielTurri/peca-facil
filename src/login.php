a0d40def4a6be46bee869a1f698da75c1ba83c22
Gabriel <ga-briel.t.a@hotmail.com>
2024-11-23 22:36:40 -0300

    navbar e tela de login

--- src/login.php
+++ src/login.php
@@ -1,11 +1,50 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
+  <link rel="stylesheet" href="./styles/login.css">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Peça Fácil | Login</title>
 </head>
 <body>
+<div class="navbar">
+  <a href="./index.php">
+    <div class="logo">
+      <h2>Peça Fácil</h2>  
+    </div>
+  </a>
+</div>
+
+<div class="login-container">
+  <div class="login-content">
+    <span class="sm-text">Acesse com e-mail e senha</span>
+    <div class="campo">
+      <label for="email">E-mail:</label>
+      <input type="text" id="email" placeholder="Digite seu e-mail">
+    </div>
+    <div class="campo">
+      <label for="senha">Senha:</label>
+      <input type="password" id="senha" placeholder="Digite sua senha">
+    </div>
+
+    <button class="botao-entrar" type="submit">
+        <a href="">
+        Entrar
+      </a>
+      </button>
+    <div class="opcoes">
+      <a href="#" class="sm-text">Esqueci minha senha</a>
+      <span class="sm-text">ou</span>
+      <a href="#" class="sm-text">Não tenho uma conta</a>
+    </div>
+  </div>
+
+</div>
+
   
 </body>
 </html>
